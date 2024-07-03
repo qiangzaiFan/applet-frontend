50b657350f001639b36c42c805868dbe7ffea12d
wangqiang <wangqiang@git.com.cn>
2024-07-03 15:21:56 +0800

    add json file

---
+++ assets/json/data/data.js
@@ -0,0 +1,13 @@
+// http://se.7yue.pro/v1/theme/by/names // 根据names获取banner
+// http://se.7yue.pro/v1/theme/name/t-2/with_spu //获取spu
+// http://se.7yue.pro/v1//banner/name/b-1 获取banenr-1
+// http://se.7yue.pro/v1//banner/name/b-2 获取banenr-2
+// http://se.7yue.pro/v1/category/grid/all 获取all
+// http://se.7yue.pro/v1/activity/name/a-2 获取 a-2
+// http://se.7yue.pro/v1/spu/id/2/detail 获取毛衣的detail (maoyi.json)
+// http://se.7yue.pro/v1/theme/by/names // 获取t-1-2-3-4
+
+//瀑布流
+// http://se.7yue.pro/v1/spu/latest?start=5&count=5 // slide0-5
+// http://se.7yue.pro/v1/spu/latest?start=10&count=5 //slide6-10
+// http://se.7yue.pro/v1/spu/latest?start=15&count=5 // slide10-15
